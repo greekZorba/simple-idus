f225ee50f1e98262569688ca961efe051d86622d
greekZorba <smiling.jinhak@gmail.com>
2021-10-19 18:42:43 +0900

    spring boot commit

---
+++ src/main/java/com/homework/simpleidus/SimpleIdusApplication.java
@@ -0,0 +1,13 @@
+package com.homework.simpleidus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIdusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleIdusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homework/simpleidus/SimpleIdusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.simpleidus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleIdusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
